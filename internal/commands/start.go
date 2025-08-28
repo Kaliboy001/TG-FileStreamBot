9d1c05d782d6d032ae5ded84c8117976a06287a6
Kaliboy001 <mr.shokrullah@gmail.com>
2025-08-28 19:39:03 +0430

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -8,14 +8,19 @@ import (
 	"github.com/celestix/gotgproto/dispatcher/handlers"
 	"github.com/celestix/gotgproto/ext"
 	"github.com/celestix/gotgproto/storage"
+	"github.com/celestix/gotgproto/mtproto"
+	"github.com/celestix/gotgproto/mtproto/types"
 )
 
+// Register both command and callback handlers
 func (m *command) LoadStart(dispatcher dispatcher.Dispatcher) {
 	log := m.log.Named("start")
 	defer log.Sugar().Info("Loaded")
 	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	dispatcher.AddHandler(handlers.NewCallback(callbackDev, "dev_info"))
 }
 
+// /start command handler with inline button
 func start(ctx *ext.Context, u *ext.Update) error {
 	chatId := u.EffectiveChat().GetID()
 	peerChatId := ctx.PeerStorage.GetPeerById(chatId)
@@ -26,6 +31,33 @@ func start(ctx *ext.Context, u *ext.Update) error {
 		ctx.Reply(u, "You are not allowed to use this bot.", nil)
 		return dispatcher.EndGroups
 	}
-	ctx.Reply(u, "Hi, send me any file to get a direct streamble link to that file.", nil)
+
+	// Inline button markup
+	keyboard := &types.ReplyMarkupInlineKeyboard{
+		Rows: [][]*types.KeyboardButtonInline{
+			{
+				{
+					// Text on the button and callback data
+					Text:         "Dev",
+					CallbackData: "dev_info",
+				},
+			},
+		},
+	}
+
+	ctx.Reply(u, "Hi, send me any file to get a direct streamble link to that file.", &mtproto.ReplyMarkup{
+		InlineKeyboard: keyboard,
+	})
 	return dispatcher.EndGroups
+}
+
+// Callback handler for "Dev" button
+func callbackDev(ctx *ext.Context, u *ext.Update) error {
+	cb := u.CallbackQuery()
+	if cb == nil || cb.Data != "dev_info" {
+		return nil
+	}
+	ctx.AnswerCallbackQuery(cb, nil)
+	ctx.SendMessage(cb.Message.Peer, "This bot Developed by @Kaliboy002", nil)
+	return nil
 }
