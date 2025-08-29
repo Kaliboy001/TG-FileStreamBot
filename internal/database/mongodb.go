b16dadb4c73c5f243e6fa2231ea641fdb519be79
Kaliboy001 <mr.shokrullah@gmail.com>
2025-08-29 16:00:34 +0430

    Update mongodb.go

--- internal/database/mongodb.go
+++ internal/database/mongodb.go
@@ -35,7 +35,8 @@ func Connect(mongoURI string, log *zap.Logger) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		return err
 	}
